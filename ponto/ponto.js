839473292d389aaf24118084f33ea4986cdf57fc
dmitrialves <dmitrialves@gmail.com>
2017-02-20 23:33:09 -0300

    primeiro upload

---
+++ ponto/ponto.js
@@ -0,0 +1,24 @@
+ponto = function(x, y, r, v) {
+  this.pos = createVector(x, y);
+  this.r = r;
+  this.dis = 0;
+  this.col = 0;
+  this.v = v;
+
+  this.desenha = function() {
+    this.dis = dist(this.pos.x, this.pos.y, mouseX, mouseY);
+    this.col = map(this.dis, 0, width/2, 0, 359);
+    noStroke();
+    colorMode(HSB);
+    fill(this.col, 255, 255, 0.4);
+    ellipse(this.pos.x, this.pos.y, 2 * r, 2 * r);
+  }
+
+  this.move = function() {
+    this.pos.x += random(-1 * this.v, this.v);
+    this.pos.y += random(-1 * this.v, this.v);
+    this.pos.x = constrain(this.pos.x, 0 + this.r, width - this.r);
+    this.pos.y = constrain(this.pos.y, 0 + this.r, height - this.r);
+  }
+
+}
---
+++ ponto/sketch.js
@@ -0,0 +1,19 @@
+var pontos = [];
+var numpontos = 700;
+var raio = 3;
+var velocidade = 5;
+
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+  for (var i = 0; i < numpontos; i++) {
+    pontos[i] = new ponto(floor(random(0, width)), floor(random(0, height)), 5, velocidade);
+  }
+}
+
+function draw() {
+  for (var i = 0; i < pontos.length; i++) {
+    pontos[i].desenha();
+    pontos[i].move();
+  }
+}
