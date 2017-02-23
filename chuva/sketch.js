815f3edd4e63f154575586eedc4c3a99b6bf6e41
dmitrialves <dmitrialves@gmail.com>
2017-02-23 19:11:16 -0300

    chuva1.0

---
+++ chuva/sketch.js
@@ -0,0 +1,52 @@
+var pingos = [];
+
+function setup() {
+  createCanvas(800, 400);
+  for (var i = 0; i < 800; i++) {
+    pingos[i] = new pingo(random(width), random(-500, -50));
+  }
+}
+
+function draw() {
+  background(0);
+  for (var i = pingos.length - 1; i >= 0; i--) {
+    pingos[i].update();
+    pingos[i].show();
+  }
+}
+
+var pingo = function(x, y) {
+  this.pos = createVector(x, y);
+  this.z = random(0, 50);
+  if (this.z <= 30) {
+    this.vel = map(this.z, 0, 30, 2, 3);
+    this.tam = map(this.z, 0, 30, 1, 3);
+  } else {
+    this.vel = map(this.z, 31, 50, 4, 7);
+    this.tam = map(this.z, 31, 50, 4, 8);
+  }
+
+  this.update = function() {
+    //this.vel += 0.05;
+    if (this.pos.y >= height) {
+      this.pos.x = random(width);
+      this.pos.y = random(-800, -75);
+      this.z = random(0, 50);
+      if (this.z <= 30) {
+        this.vel = map(this.z, 0, 30, 2, 3);
+        this.tam = map(this.z, 0, 30, 1, 3);
+      } else {
+        this.vel = map(this.z, 31, 50, 4, 7);
+        this.tam = map(this.z, 31, 50, 4, 8);
+      }
+    } else {
+      this.pos.y += this.vel;
+    }
+  }
+  this.show = function() {
+    var j = map(this.z, 0, 50, 1, 4);
+    strokeWeight(j);
+    stroke(0, 50, 230);
+    line(this.pos.x, this.pos.y, this.pos.x, this.pos.y + this.tam);
+  }
+}
