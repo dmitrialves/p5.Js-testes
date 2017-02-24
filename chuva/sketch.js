f7bee66a850e738bd7dfb4587e5301627ab32a3e
dmitrialves <dmitrialves@gmail.com>
2017-02-23 21:46:56 -0300

    Update sketch.js

--- chuva/sketch.js
+++ chuva/sketch.js
@@ -1,7 +1,7 @@
 var pingos = [];
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(600, 400);
   for (var i = 0; i < 800; i++) {
     pingos[i] = new pingo(random(width), random(-500, -50));
   }
@@ -10,8 +10,10 @@ function setup() {
 function draw() {
   background(0);
   for (var i = pingos.length - 1; i >= 0; i--) {
-    pingos[i].update();
-    pingos[i].show();
+    pingos[i].update(i);
+    if (pingos[i]) {
+      pingos[i].show();
+    }
   }
 }
 
@@ -26,19 +28,12 @@ var pingo = function(x, y) {
     this.tam = map(this.z, 31, 50, 4, 8);
   }
 
-  this.update = function() {
-    //this.vel += 0.05;
+  this.update = function(i_) {
+    this.vel += 0.02;
+    var i = i_;
     if (this.pos.y >= height) {
-      this.pos.x = random(width);
-      this.pos.y = random(-800, -75);
-      this.z = random(0, 50);
-      if (this.z <= 30) {
-        this.vel = map(this.z, 0, 30, 2, 3);
-        this.tam = map(this.z, 0, 30, 1, 3);
-      } else {
-        this.vel = map(this.z, 31, 50, 4, 7);
-        this.tam = map(this.z, 31, 50, 4, 8);
-      }
+      pingos.splice(i, 1);
+      pingos.push(new pingo(random(width), random(-500, -50)));
     } else {
       this.pos.y += this.vel;
     }
