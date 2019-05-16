ad99cede9727f881572c8dd7b4bc643a58b7a678
Dmitri Alves <dmitrialves@gmail.com>
2019-05-16 16:47:57 -0300

    Update sketch.js

--- ponto/sketch.js
+++ ponto/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(500, 500);
   background(0);
   for (var i = 0; i < numpontos; i++) {
-    pontos[i] = new ponto(floor(random(0, width)), floor(random(0, height)), random(maxraio+1), velocidade);
+    pontos[i] = new ponto(floor(random(0, width)), floor(random(0, height)), floor(random(maxraio+1)), velocidade);
   }
 }
 
